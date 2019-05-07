bac4edcde69a05259058912edf8de5f7edaeac56
Saulo Bastos <saulo1bas@gmail.com>
2019-05-07 18:25:19 -0300

    Chess Exception and Chess Position

---
+++ src/chess/ChessExpection.java
@@ -0,0 +1,18 @@
+package chess;
+
+public class ChessExpection extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ChessExpection(String message) {
+		super(message);
+		
+	}
+
+	
+
+
+}
